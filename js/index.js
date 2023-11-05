1b54744e5688f4af22dd21814c992a3ebdaacb44
A1exFox <oxoma91@gmail.com>
2023-11-05 14:28:32 +0300

    feat: header [html,scss,js] completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log('it is ready');
+const burgerbutton = document.querySelector('#burgerbutton');
+const menulist = document.querySelector('#menulist');
+
+document.addEventListener('click', clickHandler);
+
+function clickHandler(event) {
+  const triggerburger = isTriggerBurger(event.target);
+  if (triggerburger) {
+    const isActive = burgerbutton.classList.contains('_active');
+    if (!isActive && event.target == burgerbutton) {
+      burgerbutton.classList.add('_active');
+      menulist.classList.add('_active');
+      document.body.classList.add('_lock');
+      return;
+    } else {
+      burgerbutton.classList.remove('_active');
+      menulist.classList.remove('_active');
+      document.body.classList.remove('_lock');
+      return;
+    }
+  }
+}
+
+function isTriggerBurger(target) {
+  const triggers = ['.menu-item', '.header__logo-container', '.button-icon-burger']
+  const isTrigger = triggers.some(item => target.closest(item));
+  return isTrigger;
+}
